ac053aa7a55f837739fdd2a91fc4fa96fb66d781
Tom Spitz <tjspitz1986@gmail.com>
2023-04-03 12:13:18 -0500

    implement classes w/ es6

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,15 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor() {
+    super(ForagerBee);
+    this.age = 40;
+    this.job  = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+  }
+  forage() {
+    return 'I am too old, let me play cards instead';
+  }
+  gamble() {
+    this.treasureChest.push('treasure');
+  }
 };
